44ff93378b84f57d04d4af7e9a71c56dfa2a80d2
SuicidalInsanity <origamimage@gmail.com>
2025-03-19 15:30:58 -0700

    Update WaypointFollowingStrategy.cs

--- BDArmory/Competition/OrchestrationStrategies/WaypointFollowingStrategy.cs
+++ BDArmory/Competition/OrchestrationStrategies/WaypointFollowingStrategy.cs
@@ -103,7 +103,7 @@ namespace BDArmory.Competition.OrchestrationStrategies
                 yield return new WaitWhile(() => BDACompetitionMode.Instance.competitionIsActive); //DoUpdate handles the deathmatch half of the combat waypoint race and ends things when only 1 team left
             }
             else
-                yield return new WaitWhile(() => pilots.Any(pilot => pilot != null && pilot.weaponManager != null && pilot.IsRunningWaypoints && (pilot.TakingOff || !(pilot.vessel.Landed || pilot.vessel.Splashed)))); 
+                yield return new WaitWhile(() => pilots.Any(pilot => pilot != null && pilot.weaponManager != null && pilot.IsRunningWaypoints && (pilot.TakingOff || !pilot.vessel.LandedOrSplashed))); 
             var endedAt = Planetarium.GetUniversalTime();
 
             BDACompetitionMode.Instance.competitionStatus.Add("Waypoints competition finished. Scores:");
