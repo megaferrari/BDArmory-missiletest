534eac5fbfa986b9973c55fffd3cd55bf89b2d93
Brett Ryland <b.n.ryland@gmail.com>
2025-04-15 13:50:42 +0200

    Reorder some commands to clean up readability.

--- BDArmory/Control/MissileFire.cs
+++ BDArmory/Control/MissileFire.cs
@@ -1082,16 +1082,16 @@ namespace BDArmory.Control
                     if (gun == null || (gun.useThisWeaponForAim && (gun.ammoCount > 0 || BDArmorySettings.INFINITE_AMMO))) //it's a missile, or an aim-override enabled weapon with ammo
                         if (sw.GetPart().vessel == vessel) return sw;
                 }
-                IBDWeapon candidateWeapon = null; //weapon no longer on craft. Null in case below while loop doesn't find other weapons in the same group on craft.
                 //missile no longer on craft, or a gun that isn't aim overridden, or sw hasn't been set yet
-                if (weaponIndex <= 0)
+                if (weaponIndex <= 0) //no weapon selected
                 {
                     selectedWeapon = null;
-                    return sw; //no weapon selected
+                    return sw;
                 }
                 //if ((sw != null && sw.GetPart().vessel == vessel) || weaponIndex <= 0) return sw; //this is going to return the first gun of a weaponGroup, regardless of overheat/reload state, as long as gun was valid when first selected
                 // should only apply if selected weapon is missile/bomb/slw
 
+                IBDWeapon candidateWeapon = null; //weapon no longer on craft. Null in case below while loop doesn't find other weapons in the same group on craft.
                 IBDWeapon candidateGun = null;
                 float candidateMuzVel = 0;
 
