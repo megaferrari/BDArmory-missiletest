d3882a3843e20dfd74c9e66587cfac8c7e1fb5a3
BillNyeTheIE <BillNyeTheIlluminatiEye123@protonmail.com>
2025-05-22 16:27:33 -0700

    More OnStandbyAngleChanged Fixes
    - Reduce unnecessary runs when loading vessel

--- BDArmory/WeaponMounts/ModuleTurret.cs
+++ BDArmory/WeaponMounts/ModuleTurret.cs
@@ -38,7 +38,7 @@ namespace BDArmory.WeaponMounts
         [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = true, guiName = "#LOC_BDArmory_YawStandbyAngle"),
          UI_FloatRange(minValue = -90f, maxValue = 90f, stepIncrement = 0.5f, scene = UI_Scene.All, affectSymCounterparts = UI_Scene.None)]
         public float yawStandbyAngle = 0;
-        Quaternion standbyLocalRotation = Quaternion.identity;
+        Quaternion standbyLocalRotation;// = Quaternion.identity;
 
         [KSPField(isPersistant = true)] public float minPitchLimit = 400;
         [KSPField(isPersistant = true)] public float maxPitchLimit = 400;
@@ -369,25 +369,29 @@ namespace BDArmory.WeaponMounts
             yawStandbyAngleEd.maxValue = yawRange / 2f;
             yawStandbyAngle = Mathf.Clamp(yawStandbyAngle, yawStandbyAngleEd.minValue, yawStandbyAngleEd.maxValue);
             yawStandbyAngleEd.onFieldChanged = OnStandbyAngleChanged;
-            OnStandbyAngleChanged();
+            SetStandbyAngle();
         }
         void OnStandbyAngleChanged(BaseField field = null, object obj = null)
         {
-            SetStandbyAngle();
-            foreach (Part symmetryPart in part.symmetryCounterparts)
+            if (standbyLocalRotation != null)
             {
-                ModuleTurret symmetryTurret = symmetryPart.FindModuleImplementing<ModuleTurret>();
-                if (part.symMethod == SymmetryMethod.Mirror)
+                foreach (Part symmetryPart in part.symmetryCounterparts)
                 {
-                    symmetryTurret.yawStandbyAngle = -yawStandbyAngle;
-                }
-                else
-                {
-                    symmetryTurret.yawStandbyAngle = yawStandbyAngle;
-                }
+                    ModuleTurret symmetryTurret = symmetryPart.FindModuleImplementing<ModuleTurret>();
+                    if (part.symMethod == SymmetryMethod.Mirror)
+                    {
+                        symmetryTurret.yawStandbyAngle = -yawStandbyAngle;
+                    }
+                    else
+                    {
+                        symmetryTurret.yawStandbyAngle = yawStandbyAngle;
+                    }
 
-                symmetryTurret.SetStandbyAngle();
+                    symmetryTurret.SetStandbyAngle();
+                }
             }
+            SetStandbyAngle();
+
         }
 
         void SetStandbyAngle()
