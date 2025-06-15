d3ebf38f96129c586ce6016e46136e3c6d2f5fb6
BillNyeTheIE <BillNyeTheIlluminatiEye123@protonmail.com>
2025-06-15 15:45:47 -0700

    Partial Revert "Additional Fixes for Datalink Behavior"
    This partially reverts commit 5b88aaf86ee16de78c4b3143aa8a9f83b6f14794.
    - Moved the datalink queued launches stuff back to the missiles instead of GMR, if a datalinked craft is trying to launch at the same time as the craft with the radar, the craft with the radar should get priority.

--- BDArmory/Weapons/Missiles/MissileLauncher.cs
+++ BDArmory/Weapons/Missiles/MissileLauncher.cs
@@ -1285,6 +1285,12 @@ namespace BDArmory.Weapons.Missiles
                     {
                         if (wpm)
                             wpm.UpdateQueuedLaunches(targetVessel, this, true);
+                        if (radarTarget.exists && radarTarget.lockedByRadar && radarTarget.lockedByRadar.vessel != SourceVessel)
+                        {
+                            MissileFire datalinkwpm = VesselModuleRegistry.GetMissileFire(radarTarget.lockedByRadar.vessel, true);
+                            if (datalinkwpm)
+                                datalinkwpm.UpdateQueuedLaunches(targetVessel, this, true, false);
+                        }
                         multiLauncher.fireMissile();
                     }
                     launched = true;
@@ -1298,6 +1304,12 @@ namespace BDArmory.Weapons.Missiles
                         {
                             if (wpm)
                                 wpm.UpdateQueuedLaunches(targetVessel, this, true);
+                            if (radarTarget.exists && radarTarget.lockedByRadar && radarTarget.lockedByRadar.vessel != SourceVessel)
+                            {
+                                MissileFire datalinkwpm = VesselModuleRegistry.GetMissileFire(radarTarget.lockedByRadar.vessel, true);
+                                if (datalinkwpm)
+                                    datalinkwpm.UpdateQueuedLaunches(targetVessel, this, true, false);
+                            }
                             reloadableMissile = StartCoroutine(FireReloadableMissile());
                         }
                         launched = true;
@@ -1341,6 +1353,12 @@ namespace BDArmory.Weapons.Missiles
                     {
                         if (wpm)
                             wpm.UpdateQueuedLaunches(targetVessel, this, true);
+                        if (radarTarget.exists && radarTarget.lockedByRadar && radarTarget.lockedByRadar.vessel != SourceVessel)
+                        {
+                            MissileFire datalinkwpm = VesselModuleRegistry.GetMissileFire(radarTarget.lockedByRadar.vessel, true);
+                            if (datalinkwpm)
+                                datalinkwpm.UpdateQueuedLaunches(targetVessel, this, true, false);
+                        }
                         reloadableMissile = StartCoroutine(FireReloadableMissile());
                     }
                     launched = true;
